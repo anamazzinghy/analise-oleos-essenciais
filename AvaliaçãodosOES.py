19955d0409a0384de75b08d344036801aed6aa39
anamazzinghy <anamazzinghy@yahoo.com.br>
2025-01-21 21:31:46 -0300

    Update AvaliaçãodosOES.py

--- AvaliaçãodosOES.py
+++ AvaliaçãodosOES.py
@@ -1,5 +1,3 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> import streamlit as st
 ... import pandas as pd
 ... import numpy as np
